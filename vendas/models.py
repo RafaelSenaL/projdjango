2fd644095c517afe297fb6c062fc6691dd3c661e
RafaelSenaL <rafael.sena@aluno.senac.rio>
2026-04-23 20:10:06 -0300

    aa

--- vendas/models.py
+++ vendas/models.py
@@ -1,7 +1,11 @@
+#models
+
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     nome = models.CharField(max_length=100)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE, default= 1)
     marca = models.CharField(max_length=50, default='Genérica')
     preço = models.FloatField(default=0)
 
--- vendas/views.py
+++ vendas/views.py
@@ -1,15 +1,16 @@
 #views
 
 from django.shortcuts import render, redirect, get_object_or_404
+from django.core.exceptions import PermissionDenied
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Product
 from .forms import ProductForm
 
 def index(request):
-    product = Product.objects.all()
-
+    lista_produtos = Product.objects.all()
     contexto = {
-        'product': product,
+        'produtos': lista_produtos,
     }
     return render(request, 'vendas/index.html', contexto)
 
@@ -21,32 +22,47 @@ def new(request):
     if request.method == 'POST':
         form = ProductForm(request.POST)
         if form.is_valid():
-            form.save()
+            product = form.save(commit=False)
+            product.autor = request.user 
+            product.save() 
+            
+            messages.success(request, 'Produto adicionado com sucesso')
             return redirect('index')
     else:
         form = ProductForm()
     return render(request, 'vendas/new.html', {'form': form})
 
 def details(request, id):
-    product = get_object_or_404(Product, id=id)
-    return render(request, 'vendas/details.html', {'product': product})
+    p = get_object_or_404(Product, id=id)
+    return render(request, 'vendas/details.html', {'product': p})
 
 @login_required
 def edit(request, id):
-    product = get_object_or_404(Product, id=id)
+    p = get_object_or_404(Product, id=id)
+
+    if not (request.user.is_staff or p.autor == request.user):
+        raise PermissionDenied
+
     if request.method == 'POST':
-        form = ProductForm(request.POST, instance=product)
+        form = ProductForm(request.POST, instance=p)
         if form.is_valid():
             form.save()
-            return redirect('detalhe', id=id)
+            messages.success(request, 'produto editado com sucesso')
+            return redirect('details', id=id) 
     else:
-        form = ProductForm(instance=product)
-    return render(request, 'vendas/edit.html', {'form': form, 'product': product})
+        form = ProductForm(instance=p)
+   
+    return render(request, 'vendas/edit.html', {'form': form, 'product': p})
 
 @login_required
 def delete(request, id):
-    product = get_object_or_404(Product, id=id)
+    p = get_object_or_404(Product, id=id)
+
+    if not (request.user.is_staff or p.autor == request.user):
+        raise PermissionDenied
+
     if request.method == 'POST':
-        product.delete()
+        p.delete()
+        messages.success(request, 'produto apagado com sucesso')
         return redirect('index')
-    return render(request, 'vendas/delete.html', {'product': product})
+    return render(request, 'vendas/delete.html', {'product': p})
